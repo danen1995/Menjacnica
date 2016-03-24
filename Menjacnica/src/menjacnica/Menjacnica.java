95f705b6d9915b43e62830f9ebc55c177edb5372
Dane <Dane@Dane>
2016-03-24 01:21:41 +0100

    Napravljena klasa koja implementira sve metode iz interfejsa.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,15 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Menjacnica {
+	public void dodajKursValute(String nazivValute, GregorianCalendar datum, double prodajni, double srednji, double kupovni){
+		
+	}
+	public void izbrisiKursValute(String nazivValute, GregorianCalendar datum){
+		
+	}
+	public Kurs pronadjiKursValute(String nazivValute, GregorianCalendar datum){
+		return null;
+	}
+}
