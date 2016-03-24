01c5764cf5db928a6d520dcb52c094af69330871
Dane <Dane@Dane>
2016-03-24 13:50:08 +0100

    Napravljena alternativna implementacija metode za dodavanje kursa valute u okviru master grane.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,11 +1,36 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 public class Menjacnica {
-	public void dodajKursValute(String nazivValute, GregorianCalendar datum, double prodajni, double srednji, double kupovni){
-		
+	
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
+	public LinkedList<Valuta> getValute() {
+		return valute;
 	}
+	
+	public void setValute(LinkedList<Valuta> valute) {
+		this.valute = valute;
+	}
+	
+	public void dodajKursValute(String nazivValute, GregorianCalendar datum, double prodajni, double srednji, double kupovni){
+		Kurs k = new Kurs();
+		k.setSrednjiKurs(srednji);
+		k.setDatum(datum);
+		k.setKupovniKurs(kupovni);
+		k.setProdajniKurs(prodajni);
+		if(nazivValute == null || datum == null){
+			 throw new RuntimeException("Greska.");
+		}
+ 		for (int i = 0; i < valute.size(); i++) {
+			 	if(valute.get(i).getNaziv().equals(nazivValute)){
+			 			valute.get(i).getKursevi().add(k);
+			 	}
+		}
+	}
+	
 	public void izbrisiKursValute(String nazivValute, GregorianCalendar datum){
 		
 	}
